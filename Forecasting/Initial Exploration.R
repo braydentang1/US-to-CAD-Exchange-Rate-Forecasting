337c7b5c7d85afe390430787690d4d9e204454f4
braydentang1 <brayden.tang1@gmail.com>
2019-05-01 21:25:14 -0600

    Update Initial Exploration.R

--- Forecasting/Initial Exploration.R
+++ Forecasting/Initial Exploration.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(forecast)
+library(caret)
 
 #Will rename once we settle on some sort of methodology
 
@@ -19,117 +20,135 @@ autoplot(diff(data.ts), ts.colour = "black", ts.size = 1) +labs(title = "US/CAD 
 #Exclude bagged ETS by default since it takes a long time to run.
 #Exclude is a character vector that will exclude the fitted models in the vector. Example: c("ETS", "Arima") excludes ETS and Arima from the results.
 
-evaluate.Models = function(data, exclude = "Bagged ETS"){
+evaluate.Models = function(data, exclude = "Bagged ETS", timeSlices){
   
-  test = window(data, start = 2018)
+  allTrain = timeSlices$train
+  allTest = timeSlices$test
   
-  error.ETS = vector("numeric", length(test))
-  error.Arima = vector("numeric", length(test))
-  error.theta = vector("numeric", length(test))
-  error.rwd = vector("numeric", length(test))
-  error.tbats = vector("numeric", length(test))
-  error.baggedETS = vector("numeric", length(test))
+  error.ETS = vector("numeric", length(allTest))
+  error.Arima = vector("numeric", length(allTest))
+  error.theta = vector("numeric", length(allTest))
+  error.rwd = vector("numeric", length(allTest))
+  error.tbats = vector("numeric", length(allTest))
+  error.baggedETS = vector("numeric", length(allTest))
+  error.STL = vector("numeric", length(allTest))
   
-  model.Ensemble = vector("list", length(test))
+  error.Ensemble = vector("numeric", length(allTest))
 
-  for(i in 1:length(test)){
+  for(i in 1:length(allTest)){
   
-    model.Average = vector("numeric", 4)
-    train = window(data, end = 2017 + (10+i)/12)
+    model.Average = vector("list", 4)
     
+    train.time = time(data)[allTrain[[i]]]
+    test.time = time(data)[allTest[[i]]]
+    
+    train = window(data, start = train.time[1], end = train.time[length(train.time)])
+    test = window(data, start = test.time[1], end = test.time[length(test.time)])
+
     #ETS
     if(!"ETS" %in% exclude){
       
     ets.model = ets(train, ic = "aicc")
-    forecast.ets = forecast(ets.model, h = 1)$mean
-    model.Average[1] = forecast.ets
-    error.ETS[i] = (forecast.ets - test[i])^2 
+    forecast.ets = forecast(ets.model, h = length(test))
+    model.Average[[1]] = forecast.ets$mean
+    error.ETS[i] = accuracy(forecast.ets, x = test)[2,2]
     
     }
     
     #Auto.Arima
     if(!"Arima" %in% exclude){
     arima.model = auto.arima(train, stepwise = TRUE, ic = "aicc", stationary = TRUE)
-    forecast.arima = forecast(arima.model, h = 1)$mean
-    error.Arima[i] = (forecast.arima - test[i])^2 
+    forecast.arima = forecast(arima.model, h = length(test))
+    error.Arima[i] = accuracy(forecast.arima, x = test)[2,2]
     }
     
     #Theta
     if(!"Theta" %in% exclude){
-    forecast.theta = thetaf(y = train, h = 1)$mean
-    model.Average[2] = forecast.theta
-    error.theta[i] = (forecast.theta - test[i])^2
+    forecast.theta = thetaf(y = train, h = length(test))
+    model.Average[[2]] = forecast.theta$mean
+    error.theta[i] = accuracy(forecast.theta, x = test)[2,2]
     }
     
     #Naive = Random Walk 
     if(!"RWD" %in% exclude){
-    forecast.rwd = rwf(y = train, h = 1, drift = TRUE)$mean
-    model.Average[3] = forecast.rwd
-    error.rwd[i] = (forecast.rwd - test[i])^2
+    forecast.rwd = rwf(y = train, h = length(test), drift = TRUE)
+    model.Average[[3]] = forecast.rwd$mean
+    error.rwd[i] = accuracy(forecast.rwd, x = test)[2,2]
     }
     
     #TBATS/BATS
     if(!"TBATS" %in% exclude){
       model.tbats = tbats(y = train, num.cores = 4)
-      forecast.tbats = forecast(model.tbats, h=1)$mean
-      model.Average[4] = forecast.tbats
-      error.tbats[i] = (forecast.tbats-test[i])^2
+      forecast.tbats = forecast(model.tbats, h=length(test))
+      model.Average[[4]] = forecast.tbats$mean
+      error.tbats[i] = accuracy(forecast.tbats, x = test)[2,2]
     }
     
     #Bagged ETS
     if(!"Bagged ETS" %in% exclude){
       model.baggedETS = baggedETS(y = train)
-      forecast.baggedETS = forecast(model.baggedETS, h=1)$mean
-      error.baggedETS[i] = (forecast.baggedETS-test[i])^2
+      forecast.baggedETS = forecast(model.baggedETS, h=length(test))
+      error.baggedETS[i] = accuracy(forecast.baggedETS, x = test)[2,2]
     }
     
+    #STL
+    if(!"STL" %in% exclude){
+      forecast.STL = stlf(y = train, h = length(test), robust = TRUE)
+      error.STL[i] = accuracy(forecast.STL, x = test)[2,2]
+    }
     
-    model.Ensemble[[i]] = model.Average
+    #Process the ensemble, take a simple average for now
+    model.Average.Process = model.Average %>% reduce(ts.intersect) %>% rowMeans(.)
+    
+    error.Ensemble[i] = accuracy(model.Average.Process, x = test)[2]
     
   }
   
-  #Process the ensembling method; take a simple average.
-  
-  Ensemble.Process = unlist(lapply(model.Ensemble, mean)) 
-  
-  list(ETS = mean(error.ETS, na.rm = TRUE)^0.5, Auto.Arima = mean(error.Arima, na.rm = TRUE)^0.5, 
-       Theta = mean(error.theta, na.rm = TRUE)^0.5, RWD = mean(error.rwd, na.rm = TRUE)^0.5,
-       TBATS = mean(error.tbats, na.rm = TRUE)^0.5, baggedETS = mean(error.baggedETS, na.rm = TRUE)^0.5,
-       Ensemble = (mean((Ensemble.Process - test)^2))^0.5)
+  list(ETS = mean(error.ETS, na.rm = TRUE), Auto.Arima = mean(error.Arima, na.rm = TRUE), 
+       Theta = mean(error.theta, na.rm = TRUE), RWD = mean(error.rwd, na.rm = TRUE),
+       TBATS = mean(error.tbats, na.rm = TRUE), baggedETS = mean(error.baggedETS, na.rm = TRUE),
+       STL = mean(error.STL, na.rm = TRUE), Ensemble = mean(error.Ensemble, na.rm = TRUE))
   
 }
 
-
 #Not used because it leads to a worse model.
-evaluate.baggedTheta = function(n, data){
+evaluate.baggedTheta = function(n, data, timeSlices){
   
-  test = window(data, start = 2018)
+  allTrain = timeSlices$train
+  allTest = timeSlices$test
   error= vector("numeric", length(test))
   
-  for(i in 1:length(test)){
+  for(i in 1:length(allTest)){
     
-    train = window(data, end = 2017 + (10+i)/12)
+    train.time = time(data)[allTrain[[i]]]
+    test.time = time(data)[allTest[[i]]]
+    
+    train = window(data, start = train.time[1], end = train.time[length(train.time)])
+    test = window(data, start = test.time[1], end = test.time[length(test.time)])
+    
     set.seed(200350623)
     bootstrap.train = bld.mbb.bootstrap(x = train, num = n)
     
-    prediction = mean(unlist(lapply(bootstrap.train, FUN = forecast.BaggedTheta)))
-    error[i] = (prediction - test[i])^2
+    predictions = lapply(bootstrap.train, FUN = forecast.BaggedTheta)
+    model.Average.Process = predictions %>% reduce(ts.intersect) %>% rowMeans(.)
+    error[i] = accuracy(model.Average.Process, x = test)[2]
     
   }
   
-  mean(error)^0.5
+  mean(error)
   
 }
 
 #Not used because it leads to a worse model.
 forecast.BaggedTheta = function(x){
   
-  forecast.theta = thetaf(y = x, h = 1)$mean
+  forecast.theta = thetaf(y = x, h = 2)$mean
 
 }
 
-#I exclude the ARIMA and Bagged ETS Models because they are significantly worse than the others
-results = evaluate.Models(data = data.ts, exclude = c("Arima", "Bagged ETS"))
 
+timeSlices = createTimeSlices(y = data.ts, initialWindow = 560, horizon = 2, fixedWindow = FALSE)
+results = evaluate.Models(data = data.ts, exclude = c("Arima", "Bagged ETS"), timeSlices = timeSlices)
+#I exclude the ARIMA and Bagged ETS Models because they are significantly worse than the others
 #Tried using this but model is much worse. Don't use.
 #results.BaggedTheta = evaluate.baggedTheta(n = 5000, data = data.ts)
