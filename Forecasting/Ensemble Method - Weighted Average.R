6c145a434191c546de89a26e71742c63022db665
braydentang1 <brayden.tang1@gmail.com>
2019-09-06 16:35:28 -0700

    update ensemble method grid search methodology

--- Forecasting/Ensemble Method - Weighted Average.R
+++ Forecasting/Ensemble Method - Weighted Average.R
@@ -2,7 +2,11 @@ library(tidyverse)
 library(forecast)
 library(caret)
 library(parallel)
+library(lubridate)
 library(alfred)
+library(parallel)
+
+source("/home/brayden/GitHub/US-to-CAD-Exchange-Rate-Forecasting/Forecasting/allFunctions.R")
 
 #0.02144643 RMSE
 #Worse than best single method RWD.
@@ -15,127 +19,25 @@ data = get_fred_series("EXCAUS", "EXCAUS") %>%
 data.ts = ts(data$EXCAUS, frequency = 12, start = c(year(data$DATE[1]), month(data$DATE[1])), end = c(year(data$DATE[nrow(data)]), month(data$DATE[nrow(data)]))) %>%
   window(., start = c(2000,1))
 
-#......................Fit the ensemble model...............................#
+#..........................................Feature Engineering..............................................................#
 
-innerTrain = function(trainFoldIndex, data, iterations){
-  
-  set.seed(200350623)
-  randomGrid = tibble(ETS.Weight = sample.int(10000, size = iterations, replace = TRUE), Theta.Weight = sample.int(10000, size = iterations, replace = TRUE),
-                      RWD.Weight = sample.int(10000, size = iterations, replace = TRUE), TBATS.Weight = sample.int(10000, size = iterations, replace = TRUE),
-                      STL.Weight = sample.int(10000, size = iterations, replace = TRUE)) %>% 
-    mutate(sumofWeights = rowSums(.)) 
-  
-  outerTrain.time = time(data)[trainFoldIndex]
-  outerTrain = window(data, start = outerTrain.time[1], end = outerTrain.time[length(outerTrain.time)])
-  
-  allInnerSets = createTimeSlices(y = outerTrain, initialWindow = 550, horizon = 3, fixedWindow = FALSE)
-  allInnerTrain = allInnerSets$train
-  allValidation = allInnerSets$test
-  
-  scores = vector("list", length(allInnerTrain))
-  
-  for(k in 1:length(allInnerTrain)){
-    
-    train.time = time(data)[allInnerTrain[[k]]]
-    train = window(data, start = train.time[1], end = train.time[length(train.time)])
-    
-    validation.time = time(data)[allValidation[[k]]]
-    validation = window(data, start = validation.time[1], end = validation.time[length(validation.time)])
-    
-    allPredictions = grabPredictions(train = train, newdata = validation) %>% 
-      reduce(ts.intersect) %>% 
-      as_tibble(.) %>%
-      set_names(c("ETS", "Theta", "RWD", "TBATS", "STL"))
-    
-    scores[[k]] = randomGridSearch(grid = randomGrid, predictions = allPredictions, validation = validation)
-    
-  }
-  
-  finalGrid = scores %>% reduce(cbind) %>% rowMeans(.) %>% bind_cols(randomGrid, Score = .)
-  finalParameters = finalGrid[which.min(finalGrid$Score), ]
-  
-  finalParameters
-}
+data.FE = data %>% 
+  mutate(GreatRecession.Ind = ifelse(DATE >= ymd("2007-12-01") & DATE <= ymd("2009-06-01"),1,0)) %>%
+  mutate(DotCom_Recession.Ind = ifelse(DATE >= ymd("2001-03-01") & DATE <= ymd("2001-11-01"), 1,0)) %>%
+  mutate(TwoThousandTen.LevelChange = ifelse(year(DATE) >= 2008, 1,0)) %>%
+  mutate(HOLIDAYS = ifelse(month(DATE) == 11 | month(DATE) == 12, 1,0)) %>%
+  mutate(TwoThousandTwo.LevelChange = ifelse(year(DATE) >= 2002.2 & year(DATE) <= 2007.5, 1,0)) %>%
+  filter(DATE >= ymd("2000-1-01")) %>%
+  select(-EXCAUS, -DATE) %>%
+  data.matrix(.) 
 
-outerFold = function(data, trainFoldsIndex, testFoldsIndex, parameters){
-  
-  outerTrain.time = time(data)[trainFoldsIndex]
-  outerTrain = window(data, start = outerTrain.time[1], end = outerTrain.time[length(outerTrain.time)])
-  
-  test.time = time(data)[testFoldsIndex]
-  test = window(data, start = test.time[1], end = test.time[length(test.time)])
-  
-  finalPredictions = grabPredictions(train = outerTrain, newdata = test) %>% 
-    reduce(ts.intersect) %>%
-    as_tibble(.) %>%
-    set_names(c("ETS", "Theta", "RWD", "TBATS", "STL"))
-  
-  score = randomGridSearch(grid = parameters, predictions = finalPredictions, validation = test)
-  
-  score
-}
+#..........................................Fit model.........................................................................#
 
+timeSlices = createTimeSlices(y = data.ts, initialWindow = 210, horizon = 3, fixedWindow = FALSE)
 
-
-grabPredictions = function(train, newdata){
-  
-  model.Average = vector("list", 5)
-  
-    ets.model = ets(train, ic = "aicc")
-    forecast.ets = forecast(ets.model, h = length(newdata))
-    model.Average[[1]] = forecast.ets$mean
-
-    forecast.theta = thetaf(y = train, h = length(newdata))
-    model.Average[[2]] = forecast.theta$mean
-    
-    forecast.rwd = rwf(y = train, h = length(newdata), drift = TRUE)
-    model.Average[[3]] = forecast.rwd$mean
-    
-    model.tbats = tbats(y = train, num.cores = 4)
-    forecast.tbats = forecast(model.tbats, h=length(newdata))
-    model.Average[[4]] = forecast.tbats$mean
-    
-    forecast.STL = stlf(y = train, h = length(newdata), robust = TRUE)
-    model.Average[[5]] = forecast.STL$mean
-    
-    model.Average
-}
-
-
-randomGridSearch = function(grid, predictions, validation){
-  
-  score = vector("numeric", nrow(grid))
-  
-  for (k in 1:nrow(grid)){
-    
-    predictions.tmp = predictions %>% transmute(Weighted.Average = ETS * as.numeric(grid[k,1]/grid[k,6]) + Theta * as.numeric(grid[k,2]/grid[k,6]) +
-                                               RWD * as.numeric(grid[k,3]/grid[k,6]) + TBATS * as.numeric(grid[k,4]/grid[k,6]) + STL * as.numeric(grid[k,5]/grid[k,6]))
-    
-    score[k] = accuracy(f = predictions.tmp$Weighted.Average, x = validation)[2]
-    
-  }
-  
-  score
-  
-}
-
-timeSlices = createTimeSlices(y = data.ts, initialWindow = 560, horizon = 3, fixedWindow = FALSE)
-
-#Run in parallel
-cluster = makeCluster(detectCores())
-setDefaultCluster(cluster)
-
-#Load packages on each cluster
-clusterEvalQ(cluster, c(library(caret), library(forecast), library(tidyverse),
-             source("C:/Users/Brayden/Documents/GitHub/US-to-CAD-Exchange-Rate-Forecasting/Main Functions/grabPredictions.R"),
-             source("C:/Users/Brayden/Documents/GitHub/US-to-CAD-Exchange-Rate-Forecasting/Main Functions/innerTrain.R"),
-             source("C:/Users/Brayden/Documents/GitHub/US-to-CAD-Exchange-Rate-Forecasting/Main Functions/outerFold.R"),
-             source("C:/Users/Brayden/Documents/GitHub/US-to-CAD-Exchange-Rate-Forecasting/Main Functions/randomGridSearch.R")))
-
-bestParameters = parLapply(NULL, timeSlices$train, fun = innerTrain, data = data.ts, iterations = 90)
-stopCluster(cluster)
+bestParameters = mclapply(X = timeSlices$train, FUN = innerTrain, data = data.ts, iterations = 90, xreg = data.FE, mc.cores = 4)
 
 #bestParameters = lapply(timeSlices$train, FUN = innerTrain, data = data.ts, iterations = 75)
 
-finalResults = mapply(FUN = outerFold, trainFoldsIndex = timeSlices$train, testFoldsIndex = timeSlices$test, parameters = bestParameters, MoreArgs = list(data = data.ts), SIMPLIFY = FALSE)
+finalResults = mapply(FUN = outerFold, trainFoldsIndex = timeSlices$train, testFoldsIndex = timeSlices$test, parameters = bestParameters, MoreArgs = list(data = data.ts, xreg = data.FE), SIMPLIFY = FALSE)
 mean(unlist(finalResults))
---
+++ Forecasting/allFunctions.R
@@ -0,0 +1,120 @@
+library(tidyverse)
+library(forecast)
+library(caret)
+library(parallel)
+library(lubridate)
+library(alfred)
+
+#......................Fit the ensemble model...............................#
+
+innerTrain = function(trainFoldIndex, data, iterations, xreg){
+  
+  set.seed(200350623)
+  randomGrid = tibble(ETS.Weight = sample.int(10000, size = iterations, replace = TRUE), Theta.Weight = sample.int(10000, size = iterations, replace = TRUE),
+                      RWD.Weight = sample.int(10000, size = iterations, replace = TRUE), TBATS.Weight = sample.int(10000, size = iterations, replace = TRUE),
+                      STL.Weight = sample.int(10000, size = iterations, replace = TRUE), ARIMA.Weight = sample.int(10000, size = iterations, replace = TRUE)) %>% 
+    mutate(sumofWeights = rowSums(.)) 
+  
+  outerTrain.time = time(data)[trainFoldIndex]
+  outerTrain = window(data, start = outerTrain.time[1], end = outerTrain.time[length(outerTrain.time)])
+  
+  allInnerSets = createTimeSlices(y = outerTrain, initialWindow = 170, horizon = 3, fixedWindow = FALSE)
+  allInnerTrain = allInnerSets$train
+  allValidation = allInnerSets$test
+  
+  scores = vector("list", length(allInnerTrain))
+  
+  for(k in 1:length(allInnerTrain)){
+    
+    train.time = time(data)[allInnerTrain[[k]]]
+    train = window(data, start = train.time[1], end = train.time[length(train.time)])
+    xreg.train = xreg[1:length(train.time), ]
+    
+    validation.time = time(data)[allValidation[[k]]]
+    validation = window(data, start = validation.time[1], end = validation.time[length(validation.time)])
+    xreg.validation = xreg[1:length(validation.time), ]
+    
+    allPredictions = grabPredictions(train = train, newdata = validation, xreg.train = xreg.train, xreg.newdata = xreg.validation) %>% 
+      reduce(ts.intersect) %>% 
+      as_tibble(.) %>%
+      set_names(c("ETS", "Theta", "RWD", "TBATS", "STL", "ARIMA"))
+    
+    scores[[k]] = randomGridSearch(grid = randomGrid, predictions = allPredictions, validation = validation)
+    
+  }
+  
+  finalGrid = scores %>% reduce(cbind) %>% rowMeans(.) %>% bind_cols(randomGrid, Score = .)
+  finalParameters = finalGrid[which.min(finalGrid$Score), ]
+  
+  finalParameters
+}
+
+outerFold = function(data, trainFoldsIndex, testFoldsIndex, parameters, xreg){
+  
+  outerTrain.time = time(data)[trainFoldsIndex]
+  outerTrain = window(data, start = outerTrain.time[1], end = outerTrain.time[length(outerTrain.time)])
+  xreg.train = xreg[1:length(outerTrain.time), ]
+  
+  test.time = time(data)[testFoldsIndex]
+  test = window(data, start = test.time[1], end = test.time[length(test.time)])
+  xreg.test = xreg[1:length(test.time), ]
+  
+  finalPredictions = grabPredictions(train = outerTrain, newdata = test, xreg.train = xreg.train, xreg.newdata = xreg.test) %>% 
+    reduce(ts.intersect) %>%
+    as_tibble(.) %>%
+    set_names(c("ETS", "Theta", "RWD", "TBATS", "STL", "ARIMA"))
+  
+  score = randomGridSearch(grid = parameters, predictions = finalPredictions, validation = test)
+  
+  score
+}
+
+
+
+grabPredictions = function(train, newdata, xreg.train, xreg.newdata){
+  
+  model.Average = vector("list", 6)
+  
+  ets.model = ets(train, ic = "aicc")
+  forecast.ets = forecast(ets.model, h = length(newdata))
+  model.Average[[1]] = forecast.ets$mean
+  
+  forecast.theta = thetaf(y = train, h = length(newdata))
+  model.Average[[2]] = forecast.theta$mean
+  
+  forecast.rwd = rwf(y = train, h = length(newdata), drift = TRUE)
+  model.Average[[3]] = forecast.rwd$mean
+  
+  model.tbats = tbats(y = train, num.cores = 1)
+  forecast.tbats = forecast(model.tbats, h=length(newdata))
+  model.Average[[4]] = forecast.tbats$mean
+  
+  forecast.STL = stlf(y = train, h = length(newdata), robust = TRUE)
+  model.Average[[5]] = forecast.STL$mean
+  
+  arima.model = auto.arima(y = train, ic = "aicc", stationary = TRUE, xreg = xreg.train)
+  forecast.arima = forecast(arima.model, h = length(newdata), xreg = xreg.newdata)
+  model.Average[[6]] = forecast.arima$mean
+  
+  model.Average
+}
+
+
+randomGridSearch = function(grid, predictions, validation){
+  
+  score = vector("numeric", nrow(grid))
+  constant = which(colnames(parameters) == "sumofWeights")
+
+  for (k in 1:nrow(grid)){
+    
+    predictions.tmp = predictions %>% transmute(Weighted.Average = ETS * as.numeric(grid[k,1]/grid[k, constant]) + Theta * as.numeric(grid[k,2]/grid[k, constant]) +
+                                                  RWD * as.numeric(grid[k,3]/grid[k, constant]) + TBATS * as.numeric(grid[k,4]/grid[k, constant]) + STL * as.numeric(grid[k,5]/grid[k,constant])
+                                                + ARIMA * as.numeric(grid[k,6]/grid[k, constant]))
+    
+    score[k] = accuracy(f = predictions.tmp$Weighted.Average, x = validation)[2]
+    
+  }
+  
+  score
+  
+}
